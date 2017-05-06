248e9ded8f78f3c5a02005a070c877041135ef29
Shirley Zhou <szhou1@andrew.cmu.edu>
2017-05-06 00:00:33 -0700

    in the process of creating sessions controller and doing login stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,19 @@ Rails.application.routes.draw do
   resources :items
   resources :purchases
   resources :item_prices
+  resources :sessions
 
   # Semi-static page routes
   get 'home' => 'home#home', as: :home
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
+
+  #authentication routes
+  get 'user/edit' => 'users#edit', as: :edit_current_user
+  get 'signup' => 'users#new', as: :signup
+  get 'logout' => 'sessions#destroy', as: :logout
+  get 'login' => 'sessions#new', as: :login
   
   # Set the root url
   root :to => 'home#home'  
